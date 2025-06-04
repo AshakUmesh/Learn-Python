951af70064564f664aa7dc577108c2f8f7d3d97d
Ashak <ashakumesh2020@gmail.com>
2025-06-04 20:18:08 +0530

    optimised the code using list comprehension

--- Day23-StatesGame/main.py
+++ Day23-StatesGame/main.py
@@ -9,7 +9,6 @@ screen.addshape(img)
 data = pandas.read_csv("29_states.csv")
 state_list = data["state"].to_list()
 
-
 map_turtle = turtle.Turtle()
 map_turtle.shape(img)
 map_turtle.penup()
@@ -18,7 +17,8 @@ map_turtle.goto(0, 0)
 guess_state = []
 
 while len(guess_state) < 29:
-    users_input_state = screen.textinput(title=f"{len(guess_state)}/29 Correct State", prompt="Enter next state name").title()
+    users_input_state = screen.textinput(title=f"{len(guess_state)}/29 Correct State",
+                                         prompt="Enter next state name").title()
     if users_input_state == "Exit":
         break
     if users_input_state in state_list:
@@ -26,19 +26,12 @@ while len(guess_state) < 29:
         t = turtle.Turtle()
         t.penup()
         state_data = data[data.state == users_input_state]
-        t.goto(state_data.x.item(),state_data.y.item())
+        t.goto(state_data.x.item(), state_data.y.item())
         t.write(users_input_state)
 
-
-remaining_state = []
-for state in state_list:
-    if state in guess_state:
-        continue
-    else:
-        remaining_state.append(state)
+remaining_state = [states for states in state_list if states not in guess_state]
 
 missing_state_name = pandas.DataFrame(remaining_state)
 missing_state_name.to_csv("states_to_learn.csv")
 
 screen.exitonclick()
-
