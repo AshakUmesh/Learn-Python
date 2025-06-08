70aaf309e29b75fad79bb837b4673b0b51f08942
Ashak <ashakumesh2020@gmail.com>
2025-06-08 13:32:10 +0530

    added exception handling if user input numerical data type

--- Day24-NatoAlphabet/main.py
+++ Day24-NatoAlphabet/main.py
@@ -3,14 +3,14 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 dic = {row.letter : row.code for (index , row) in data.iterrows()}
 
-input_name = input("Enter a word: ").upper()
-
-new_list = [letter for letter in input_name]
-
-try:
-    phonetic_list = [dic[letter] for letter in input_name]
-except KeyError:
-    print(f"Sorry, only letters in the input please")
+def gen_phenetic():
     input_name = input("Enter a word: ").upper()
-else:
-    print(phonetic_list)
+    try:
+        phonetic_list = [dic[letter] for letter in input_name]
+    except KeyError:
+        print(f"Sorry, only letters in the input please")
+        gen_phenetic()
+    else:
+        print(phonetic_list)
+
+gen_phenetic()
