89c0c25dc5a7ff1e79a071b26264f8fff10cbd22
Ashak <ashakumesh2020@gmail.com>
2025-06-08 13:21:07 +0530

    added exception handling if user input numerical data type

--- Day24-NatoAlphabet/main.py
+++ Day24-NatoAlphabet/main.py
@@ -5,9 +5,12 @@ dic = {row.letter : row.code for (index , row) in data.iterrows()}
 
 input_name = input("Enter a word: ").upper()
 
-
 new_list = [letter for letter in input_name]
-print(new_list)
 
-phonetic_list = [dic[letter] for letter in input_name]
-print(phonetic_list)
+try:
+    phonetic_list = [dic[letter] for letter in input_name]
+except KeyError:
+    print(f"Sorry, only letters in the input please")
+    input_name = input("Enter a word: ").upper()
+else:
+    print(phonetic_list)
