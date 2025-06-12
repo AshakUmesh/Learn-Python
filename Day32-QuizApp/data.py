04514dbb11370cf1c764a763323a6989e3607daa
Ashak <ashakumesh2020@gmail.com>
2025-06-12 13:56:08 +0530

    Created a quiz app

---
+++ Day32-QuizApp/data.py
@@ -0,0 +1,12 @@
+import requests
+
+parameter = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+res = requests.get("https://opentdb.com/api.php?amount=10&type=boolean")
+res.raise_for_status()
+data = res.json()
+question_data = data["results"]
+
